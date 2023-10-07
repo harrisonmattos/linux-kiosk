a729e0928cc0190b0dd5214b450076a1b7044b4f
Harrison Mattos <86637412+harrisonmattos@users.noreply.github.com>
2023-10-07 15:03:03 -0300

    Update install.sh
    Ajuste if erro

--- install.sh
+++ install.sh
@@ -40,26 +40,25 @@ array=("normal" "inverted" "right" "left")
 # Fazer um case para evitar erros de escrita
 if [[ "$var1" == 'y' ]]; then
 	read -p "Direção da rotação? [normal / inverted / right / left]: " var2
-for i in "${array[@]}"; do
-if [[ "$i" == "$var2" ]]; then
-	# create config no rotate
-	if [ -e "/etc/lightdm/lightdm.conf" ]; then
-	  mv /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.backup
-	fi
+    for i in "${array[@]}"; do
+        if [[ "$i" == "$var2" ]]; then
+        # create config no rotate
+        if [ -e "/etc/lightdm/lightdm.conf" ]; then
+            mv /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.backup
+        fi
 cat > /etc/lightdm/lightdm.conf << EOF
 [SeatDefaults]
 autologin-user=kiosk
 user-session=openbox
 display-setup-script=xrandr -o $var2
 EOF
-fi
-fi
-fi
+        fi
+    done
 else
-# create config no rotate
-if [ -e "/etc/lightdm/lightdm.conf" ]; then
-  mv /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.backup
-fi
+    # create config no rotate
+    if [ -e "/etc/lightdm/lightdm.conf" ]; then
+    mv /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.backup
+    fi
 cat > /etc/lightdm/lightdm.conf << EOF
 [SeatDefaults]
 autologin-user=kiosk
